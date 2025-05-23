a37fd714cd98b1dbbbda27ce617e52f782cf2cdd
metadream <mophisoft@gmail.com>
2025-05-23 09:56:14 +0800

    upgrade

--- start.ts
+++ start.ts
@@ -1,5 +1,5 @@
-import "jsr:@focal/docify@1.0.8";
+import "jsr:@focal/docify@1.0.10";
 
 // deno run --allow-all start.ts
-// deno install --allow-all --no-check -r -f https://deno.land/x/deploy/deployctl.ts
+// deno install -gArf jsr:@deno/deployctl
 // deployctl deploy --token=ddp_patuMYsZBJSdqE05pNnDMg8fzlyjfg4HJgk5 --project=tech-docs ./start.ts
