faa468975bf4d23c8920be1e7ad59139a902cb0c
metadream <app@zerg.cc>
2026-01-01 21:46:16 +0800

    Update start.ts

--- start.ts
+++ start.ts
@@ -1,5 +1,6 @@
 import "jsr:@focal/docify@1.0.10";
 
 // deno run --allow-all start.ts
+
 // deno install -gArf jsr:@deno/deployctl
 // deployctl deploy --token=ddp_patuMYsZBJSdqE05pNnDMg8fzlyjfg4HJgk5 --project=docify ./start.ts
